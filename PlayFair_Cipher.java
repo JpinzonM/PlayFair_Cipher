78e60a47da0ab6df8ab5649c8f50d01e99ecbaac
JpinzonM <apinzonj@uninorte.edu.co>
2019-02-01 08:24:49 -0500

    Changing the mode for reading message

--- PlayFair_Cipher.java
+++ PlayFair_Cipher.java
@@ -14,21 +14,9 @@ import java.util.Scanner;
  */
 public class PlayFair_Cipher {
 
-    private static String readmessage() {
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    private String[] abecedario = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L",
-        "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    private static String message;
-    private static String encript_message;
-    private static String Desencrypt_message;
     private static int opt;
-    private static String[] aux;
+    private static String message;
+    private static String readytoencrypt;
 
     public static void main(String[] args) {
         // TODO code application logic here
@@ -43,12 +31,39 @@ public class PlayFair_Cipher {
                 System.out.println("ingrese el mensaje a cifrar: \n");
                 message = readmessage();
                 System.out.println("message: " + message);
+                readytoencrypt = config_message(message);
+                System.out.println(readytoencrypt);
                 
                 break;
             default:
                 break;
         }
+    }
+    /***
+     * funtion that read message with some words.
+     * @return 
+     */
+    private static String readmessage() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
 
+   /***
+    * funtiont that clean and prepare message for encript 
+    * @param message
+    * @return 
+    */
+    private static String config_message(String message) {
+        String[] messagec; String encript_message="";
+        messagec = message.split(" ");
+
+
+        
+        for (int i = 0; i <= messagec.length; i++) {
+            encript_message += encript_message.concat(messagec[i]);
+        }
+        System.out.println(encript_message);
+        return message;
     }
 
 }
